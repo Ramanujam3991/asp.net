15dcc7394a820d7f6b451876608cc7d9b3f62a3e
Ramanujam3991 <ramanujam3991@gmail.com>
2021-10-31 22:09:07 -0400

    Single page application: Initial Commit

---
+++ week2_single_page_application/week2_single_page_application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace week2_single_page_application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
